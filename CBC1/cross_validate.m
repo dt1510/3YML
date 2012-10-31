a9ad3475309e48ec63604d73b28c47ee787f9b5d
vinea <va909@visual07.doc.ic.ac.uk>
2012-10-31 18:11:32 +0000

    statistics output arguments and cross_validate output args changed and bugs fixed

--- CBC1/cross_validate.m
+++ CBC1/cross_validate.m
@@ -4,6 +4,9 @@ function [ info ] = cross_validate( examples, target_vector, fold_number )
 
     num_classes = 6;
     info = struct('predictions',{{}},'confusion_matrix',{{}},'error_rate',{{}},'precision_rates',{{}},'recall_rates',{{}},'F1_measures',{{}});
+    info.precision_rates = zeros(1,num_classes);
+    info.recall_rates = zeros(1,num_classes);
+    info.F1_measures = zeros(1,num_classes);
     trees = {{},{},{},{},{},{}};
     [test_examples, test_targets, train_examples, train_targets] = split_data(examples, target_vector, fold_number); 
     for i = 1:num_classes
@@ -11,12 +14,12 @@ function [ info ] = cross_validate( examples, target_vector, fold_number )
     end
     
     info.predictions = test_trees(trees,test_examples);
-    info.confusion_matrix = get_confusion_matrix(num_classes, test_targets, predictions);
-    info.error_rate = get_error_rate(test_targets, predictions);
+    info.confusion_matrix = get_confusion_matrix(num_classes, test_targets, info.predictions);
+    info.error_rate = get_error_rate(test_targets, info.predictions);
      for i = 1:num_classes
           info.precision_rates(i) = get_precision_rate(i, info.confusion_matrix);
           info.recall_rates(i) = get_recall_rate(i, info.confusion_matrix);
-          info.F1_measures(i) = get_F1_measure(info.recall_rates(i), precision_rates(i));
+          info.F1_measures(i) = get_F1_measure(info.recall_rates(i), info.precision_rates(i));
      end
      
 end
