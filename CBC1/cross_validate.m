8d4cd85b56fcbfcfab789899a304acf6626c2a8f
vinea <va909@visual07.doc.ic.ac.uk>
2012-10-31 18:04:11 +0000

    statistics output arguments and cross_validate output arguments changed

--- CBC1/cross_validate.m
+++ CBC1/cross_validate.m
@@ -1,13 +1,23 @@
-function [ predictions, test_targets ] = cross_validate( examples, target_vector, fold_number )
+function [ info ] = cross_validate( examples, target_vector, fold_number )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
+
+    num_classes = 6;
+    info = struct('predictions',{{}},'confusion_matrix',{{}},'error_rate',{{}},'precision_rates',{{}},'recall_rates',{{}},'F1_measures',{{}});
     trees = {{},{},{},{},{},{}};
     [test_examples, test_targets, train_examples, train_targets] = split_data(examples, target_vector, fold_number); 
-    for i = 1:6
+    for i = 1:num_classes
         trees{i} = gen_tree(train_examples, train_targets, i);
     end
     
-    predictions = test_trees(trees,test_examples);
-
+    info.predictions = test_trees(trees,test_examples);
+    info.confusion_matrix = get_confusion_matrix(num_classes, test_targets, predictions);
+    info.error_rate = get_error_rate(test_targets, predictions);
+     for i = 1:num_classes
+          info.precision_rates(i) = get_precision_rate(i, info.confusion_matrix);
+          info.recall_rates(i) = get_recall_rate(i, info.confusion_matrix);
+          info.F1_measures(i) = get_F1_measure(info.recall_rates(i), precision_rates(i));
+     end
+     
 end
 
--- CBC1/statistics.m
+++ CBC1/statistics.m
@@ -8,21 +8,21 @@ function [ stats ] = statistics( examples, target_vector )
     num_folds = 10;
   
     classifier = struct('predictions',{{}},'confusion_matrix',{{}},'error_rate',{{}},'precision_rates',{{}}, 'recall_rates',{{}},'F1_measures',{{}});
-    classifier.confusion_matrix = zeros(num_classes,num_classes);
-    classifier.precision_rates = zeros(1,num_classes);
-    classifier.recall_rates = zeros(1,num_classes);
-    classifier.F1_measures = zeros(1,num_classes);
+    %classifier.confusion_matrix = zeros(num_classes,num_classes);
+    %classifier.precision_rates = zeros(1,num_classes);
+    %classifier.recall_rates = zeros(1,num_classes);
+    %classifier.F1_measures = zeros(1,num_classes);
     
     randomise_rows(examples);
     for i = 1:num_folds
-        [predictions, test_vector] = cross_validate(examples, target_vector, i);
-        classifier(i).predictions = predictions;
-        classifier(i).confusion_matrix = get_confusion_matrix(num_classes, test_vector, classifier(i).predictions);
-        classifier(i).error_rate = get_error_rate(test_vector, classifier(i).predictions);
+        info = cross_validate(examples, target_vector, i);
+        classifier(i).predictions = info.predictions;
+        classifier(i).confusion_matrix = info.confusion_matrix;  %get_confusion_matrix(num_classes, test_vector, classifier(i).predictions);
+        classifier(i).error_rate = info.error_rate; %get_error_rate(test_vector, classifier(i).predictions);
         for j = 1:num_classes
-            classifier(i).precision_rates(j) = get_precision_rate(j, classifier(i).confusion_matrix);
-            classifier(i).recall_rates(j) = get_recall_rate(j, classifier(i).confusion_matrix);
-            classifier(i).F1_measures(j) = get_F1_measure(classifier(i).recall_rates(j), classifier(i).precision_rates(j));
+            classifier(i).precision_rates = info.precision_rates;  %get_precision_rate(j, classifier(i).confusion_matrix);
+            classifier(i).recall_rates = info.recall_rates; %get_recall_rate(j, classifier(i).confusion_matrix);
+            classifier(i).F1_measures = info.F1_measures; %get_F1_measure(classifier(i).recall_rates(j), classifier(i).precision_rates(j));
         end        
     end
     
