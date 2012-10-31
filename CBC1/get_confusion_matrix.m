606668b03da7c39f359225071bbc00436a9b375d
vinea <va909@visual07.doc.ic.ac.uk>
2012-10-31 17:15:57 +0000

    statistics_completed

--- CBC1/get_confusion_matrix.m
+++ CBC1/get_confusion_matrix.m
@@ -16,7 +16,6 @@ function conf_matrix = get_confusion_matrix( n, actual_labels, predicted_labels 
 % for assignment 2 (decision trees), n = 6
 
 conf_matrix = zeros(n,n);
-
 for i = 1:length(actual_labels)
     if actual_labels(i) == predicted_labels(i)
         conf_matrix(actual_labels(i), actual_labels(i)) = conf_matrix(actual_labels(i), actual_labels(i)) + 1;
--- CBC1/statistics.m
+++ CBC1/statistics.m
@@ -6,7 +6,6 @@ function [ confusion_matrix, avg_classification_rate, avg_recall_rates, avg_prec
     num_folds = 10;
   
     classifier = struct('predictions',{{}},'confusion_matrix',{{}},'error_rate',{{}},'precision_rates',{{}}, 'recall_rates',{{}},'F1_measures',{{}});
-    classifier.predictions = zeros(1,num_folds);
     classifier.confusion_matrix = zeros(num_classes,num_classes);
     classifier.precision_rates = zeros(1,num_classes);
     classifier.recall_rates = zeros(1,num_classes);
@@ -14,18 +13,23 @@ function [ confusion_matrix, avg_classification_rate, avg_recall_rates, avg_prec
     
     randomise_rows(examples);
     for i = 1:num_folds
-        classifier(i).predictions = cross_validate(examples, target_vector, i);
-        classifier(i).confusion_matrix = get_confusion_matrix(num_classes, target_vector, classifier(i).predictions);
-        classifier(i).error_rate = get_error_rate(target_vector, classifier(i).predictions);
+        [predictions, test_vector] = cross_validate(examples, target_vector, i);
+        classifier(i).predictions = predictions;
+        classifier(i).confusion_matrix = get_confusion_matrix(num_classes, test_vector, classifier(i).predictions);
+        classifier(i).error_rate = get_error_rate(test_vector, classifier(i).predictions);
         for j = 1:num_classes
             classifier(i).precision_rates(j) = get_precision_rate(j, classifier(i).confusion_matrix);
             classifier(i).recall_rates(j) = get_recall_rate(j, classifier(i).confusion_matrix);
-            classifier(i).F1_measures(j) = get_F1_measure(classifier.recall_rates(j), classifier.precision_rates(j));
+            classifier(i).F1_measures(j) = get_F1_measure(classifier(i).recall_rates(j), classifier(i).precision_rates(j));
         end        
     end
     
-    confusion_matrix = {};
-    avg_classification_rate = 1 - sum(classifer.error_rate)/num_folds;
+    confusion_matrix = zeros(num_classes,num_classes);
+    for i = 1:num_folds
+        confusion_matrix = confusion_matrix + classifier(i).confusion_matrix;
+    end
+    confusion_matrix = confusion_matrix / num_folds;
+    avg_classification_rate = 1 - sum([classifier.error_rate])/num_folds;
     avg_recall_rates = zeros(1,num_classes);
     avg_precision_rates = zeros(1,num_classes);
     avg_F1_measures = zeros(1,num_classes);
@@ -33,7 +37,7 @@ function [ confusion_matrix, avg_classification_rate, avg_recall_rates, avg_prec
         for j = 1:num_folds
             avg_precision_rates(i) = avg_precision_rates(i) + classifier(j).precision_rates(i);
             avg_recall_rates(i) = avg_recall_rates(i) + classifier(j).recall_rates(i);
-            avg_F1_measures = avg_F1_measures(i) + classifier(j).F1_measures(i);
+            avg_F1_measures(i) = avg_F1_measures(i) + classifier(j).F1_measures(i);
         end
         avg_precision_rates(i) = avg_precision_rates(i) / num_folds;
         avg_recall_rates(i) = avg_recall_rates(i) / num_folds;
