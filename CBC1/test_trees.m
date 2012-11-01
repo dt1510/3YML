d87cb7685e740beaa88d16d9a671050af5a4f83d
David Toth <dt1510@ic.ac.uk>
2012-11-01 10:49:05 +0000

    Removed legacy code: predict_binary_targets.

--- CBC1/test_trees.m
+++ CBC1/test_trees.m
@@ -5,11 +5,6 @@ function [ predicted_targets ] = test_trees( trees, examples )
 % which is then used to predict the emotion label (from 1 to 6).
 % Returns a column vector (predicted_targets) which contains the predicted
 % labels for all the examples in the test set.
-% Uses the predict_binary_targets function as an auxiliary function.
-% predicted_binary_targets is an nxm matrix that stores the 6 binary targets
-% from the trees, for each example. n is the number of examples, m is the
-% number of binary targets (in this case, m=6)
-% targets
 
     %combine outputs
     for i = 1:size(examples, 1)
