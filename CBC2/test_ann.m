e33b00621ce552ca9830c96fb189eb39dd78f654
David Toth <dt1510@ic.ac.uk>
2012-11-14 13:41:20 +0000

    Correcting test_ann.m.

--- CBC2/test_ann.m
+++ CBC2/test_ann.m
@@ -2,15 +2,13 @@ function [ classification_rate, predictions ] = test_ann( x, y, neurons )
 %TEST_NETWORK Summary of this function goes here
 %   Detailed explanation goes here
     [x2, y2] = ANNdata(x, y);
-    [net] = feedforwardnet([neurons],'traingd');
+    [net] = feedforwardnet([neurons],'trainlm');
     [net] = configure(net, x2, y2);
     [net] = train(net, x2, y2);
     
     [t] = sim(net, x2);
-    
-    classification_rate = 1- sum(sum(abs(y2 - round(t))))/size(y,1);
-    
     predictions = testANN(net,x2);
+    classification_rate = (size(y,1) - nnz(predictions - y))/size(y, 1);
     
 end
 
