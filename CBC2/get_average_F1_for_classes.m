d402664aeda36413dfe7c8e0b8a82c71cc69eb99
vinea <va909@matrix08.doc.ic.ac.uk>
2012-11-16 18:30:57 +0000

    added get_average_F1

---
+++ CBC2/get_average_F1_for_classes.m
@@ -0,0 +1,10 @@
+function [ y ] = get_average_F1_for_classes( conf_matrix )
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+    for i = 1:6
+        
+    end
+
+end
+
---
+++ CBC2/plot_F1_measures.m
@@ -0,0 +1,8 @@
+function [ ] = plot_F1_measures( y )
+%UNTITLED3 Summary of this function goes here
+%   Detailed explanation goes here
+    x = 1:10;
+    bar(x,y);
+
+end
+
