caa9eed4b7861e5d45d166d26d8d124ae9bbf253
vinea <va909@matrix08.doc.ic.ac.uk>
2012-11-16 15:43:15 +0000

    splitting training data and validation data

--- CBC2/gen_six_ann.m
+++ CBC2/gen_six_ann.m
@@ -5,6 +5,7 @@ function [ net ] = gen_six_ann( x, y )
     for i=1:6,
         net{i} = gen_bare_ann();
         [net{i}] = configure(net{i}, x2, y2(i,:));
+        [net{i}] = split_training_val(net{i});
         [net{i}] = train(net{i}, x2, y2(i,:));
     end
     
