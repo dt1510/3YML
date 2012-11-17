d2473531ae030ba37a42f1bc2339a8ea52f54149
David Toth <dt1510@ic.ac.uk>
2012-11-17 13:08:58 +0000

    Modified gen_anns.

--- CBC2/gen_anns.m
+++ CBC2/gen_anns.m
@@ -1,19 +1,20 @@
-function [ net ] = gen_anns()
+function [ anns ] = gen_anns()
 %generates an untrained ann.
 
-    activation_functions = ['trainlm'];
-    neurons = [5, 10, 20, 50, 100];
+    activation_functions = {'trainlm', 'traingd'};
+    neurons = [5, 10, 20, 50, 100];    
     hidden_layers = [1, 2, 3];
-    net = zeros[size(activation_functions)];
-    i=1;
-    for i1=1:1:size(activation_functions)
-        for i2=1:1:size(neurons)
-            for i3=1:1:size(hidden_layers)
-                layers_vector = neurons(i2)*ones(1, hidden_layers(i3));                
-                [net] = feedforwardnet(layers_vector,activation_functions(i1));
+    anns = cell(size(activation_functions,2)*size(neurons,2)*size(hidden_layers,2), 1);
+    i=0;
+    for i1=1:1:size(activation_functions,2)        
+        for i2=1:1:size(neurons,2)
+            for i3=1:1:size(hidden_layers,2)                
+                i = i + 1;
+                layers_vector = neurons(i2)*ones(1, hidden_layers(i3));
+                [net] = feedforwardnet(layers_vector,activation_functions{i1});
+                anns{i} = net;
             end
         end
     end
-
 end
 
