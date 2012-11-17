bfebd28a98fccbab2b15cb4a23db9cb338d306aa
vinea <va909@matrix21.doc.ic.ac.uk>
2012-11-17 12:12:54 +0000

    vineas updates

--- CBC2/cross_validate_one_output.m
+++ CBC2/cross_validate_one_output.m
@@ -1,24 +1,20 @@
-function [ info ] = cross_validate( x, y, fold_number )
+function [ info ] = cross_validate_one_output( x, y, fold_number, net )
 % does one fold given a fold number between 1 and 10
-%   trains 6 trees and tests predicted labels against actual labels
+%   trains a network and tests predicted labels against actual labels
 %   outputs a struct which stores predictions vector, confusion matrix,
 %   error rate for the classifier, and precision rates, recall rates and
 %   F1_measures in row vectors for each class
 
     [x2, y2] = ANNdata(x, y);
     num_classes = 6;
-    net = cell(1,6);
     info = struct('predictions',[],'confusion_matrix',[],'error_rate',[],'precision_rates',[],'recall_rates',[],'F1_measures',[]);
     info.precision_rates = zeros(1,num_classes);
     info.recall_rates = zeros(1,num_classes);
     info.F1_measures = zeros(1,num_classes);
     data = split_data(x, y, fold_number); 
-    for i = 1:num_classes
-        net{i} = gen_one_ann(x, y);
-        predictions = testANN(net,x2);
-    end
-    
-    info.predictions = 
+
+    test_examples = data.test_examples';
+    info.predictions = testANN(net,test_examples);
     info.confusion_matrix = get_confusion_matrix(num_classes, data.test_targets, info.predictions);
     info.error_rate = get_error_rate(data.test_targets, info.predictions);
      for i = 1:num_classes
