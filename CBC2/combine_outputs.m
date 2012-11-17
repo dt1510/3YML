2a90076872797eeb94a1ee2787ca54ea9af0e876
vinea <va909@matrix21.doc.ic.ac.uk>
2012-11-17 13:00:07 +0000

    cross_val

--- CBC2/combine_outputs.m
+++
@@ -1,8 +0,0 @@
-function [ predictions ] = combine_outputs( outputs )
-% general classifier for combining 6 outputs into one label from 1 - 6
-% predictions is a row vector of the predicted labels
-
-    outputs
-
-end
-
