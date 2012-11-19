976a50c6317f61a4900a8c3afa201aa2a1032a8c
Tina Liu <t.liuxox@gmail.com>
2012-11-19 11:31:15 +0000

    fixing errors

--- CBC2/cross_validate.m
+++ CBC2/cross_validate.m
@@ -10,14 +10,14 @@ function [ info ] = cross_validate( x, y, fold_number, net )
     data = split_data(x, y, fold_number);
     if size(net,1) == 6
         for i = 1:6
-            [net{i}] = train(net{i}, data.train_examples, data.train_targets);
+            [net{i}] = train(net{i}, data.train_examples', data.train_targets');
         end
     else
-        net = train(net, data.train_examples, data.train_targets);
+        net = train(net, data.train_examples', data.train_targets');
     end
     test_examples = data.test_examples';
     info.predictions = testANN(net,test_examples);
-    info.confusion_matrix = get_confusion_matrix(num_classes, data.test_targets, info.predictions');
+    info.confusion_matrix = get_confusion_matrix(num_classes, data.test_targets, info.predictions);
     info.error_rate = get_error_rate(data.test_targets, info.predictions');
      for i = 1:num_classes
           info.precision_rates(i) = get_precision_rate(i, info.confusion_matrix);
