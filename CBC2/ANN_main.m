316fa72dad698c63632dd2a20892f025b16837ce
Tina Liu <t.liuxox@gmail.com>
2012-11-19 11:16:40 +0000

    removed tr output from gen_one_ann

--- CBC2/ANN_main.m
+++ CBC2/ANN_main.m
@@ -4,7 +4,7 @@ function [ ] = ANN_main( x, y )
 %assignment.
 
 %[x2, y2] = ANNdata(x,y);
-[net1, ~] = gen_one_ann(x,y);     % tr ever used??
+[net1] = gen_one_ann(x,y);     % tr ever used??
 [net6] = gen_six_ann(x,y);
 stats1 = get_stats(x, y, net1);
 stats6 = get_stats(x, y, net6);
