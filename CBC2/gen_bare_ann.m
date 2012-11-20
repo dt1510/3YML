4539f595e70d547cd98dd24ed60a6de08d31071b
vinea <va909@visual01.doc.ic.ac.uk>
2012-11-20 12:50:51 +0000

    vineas

--- CBC2/gen_bare_ann.m
+++ CBC2/gen_bare_ann.m
@@ -2,7 +2,7 @@ function [ net ] = gen_bare_ann()
 
 %generates an untrained ann. 
 
-    [net] = feedforwardnet([10],'trainlm');
+    [net] = feedforwardnet([15],'trainlm');
     net.layers{1}.transferFcn = 'tansig';
     %net.trainParam.lr = 0.2;
 end
