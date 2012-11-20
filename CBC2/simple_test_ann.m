977a5c2d9dec03c9b3b21baed5214e6ba2290247
David Toth <dt1510@ic.ac.uk>
2012-11-20 20:28:44 +0000

    Added graphs for 6 output ANNs.

--- CBC2/simple_test_ann.m
+++ CBC2/simple_test_ann.m
@@ -1,7 +1,7 @@
 function [ net ] = simple_test_ann( x, y )
 %SIMPLE_TEST_ANN
     [x2, y2] = ANNdata(x,y);
-    net = feedforwardnet([10],'trainlm');
+    net = feedforwardnet([40, 40],'trainlm');
     net.layers{1}.transferFcn = 'tansig';    
     configure(net, x2, y2);
     net.divideParam.trainRatio = 67/100;
