924e0bf2f1d708aa2eb3267ff6bef5b4441a2799
David Toth <dt1510@ic.ac.uk>
2012-11-20 12:55:02 +0000

    simple_test_ann added.

---
+++ CBC2/simple_test_ann.m
@@ -0,0 +1,12 @@
+function [ net ] = simple_test_ann( x, y )
+%SIMPLE_TEST_ANN
+    [x2, y2] = ANNdata(x,y);
+    net = feedforwardnet([10],'trainlm');
+    net.layers{1}.transferFcn = 'tansig';    
+    configure(net, x2, y2);
+    net.divideParam.trainRatio = 67/100;
+    net.divideParam.valRatio = 33/100;
+    net.divideParam.testRatio = 0;
+    [net, tr] = train(net, x2, y2);
+end
+
