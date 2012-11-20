b460ddd9fae082379af29de05581571a3ad6469c
Tina Liu <t.liuxox@gmail.com>
2012-11-20 11:06:47 +0000

    fixed a merge

--- CBC2/gen_bare_ann.m
+++ CBC2/gen_bare_ann.m
@@ -1,10 +1,7 @@
 function [ net ] = gen_bare_ann()
 
-<<<<<<< HEAD
-=======
 %generates an untrained ann. 
 
->>>>>>> 7ccba8c4fc0e610105e6c7d6ab23b39787c9d35a
     [net] = feedforwardnet([10],'trainlm');
     net.layers{1}.transferFcn = 'tansig';
     %net.trainParam.lr = 0.2;
