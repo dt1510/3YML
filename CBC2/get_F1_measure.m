0450556d16dca372b7d06a409470d11602dfab93
vinea <va909@matrix22.doc.ic.ac.uk>
2012-11-21 14:17:56 +0000

    F1-Measure changed

--- CBC2/get_F1_measure.m
+++ CBC2/get_F1_measure.m
@@ -2,7 +2,12 @@ function F1_measure = get_F1_measure( recall_rate, precision_rate )
 % Calculates the F1 measure for a specified class based on the previously
 % calculated recall rate and precision rate
 
-F1_measure = (2*precision_rate*recall_rate) / (precision_rate + recall_rate);
+    if (recall_rate + precision_rate == 0)
+        F1_measure = 0; 
+
+    else
+        F1_measure = (2*precision_rate*recall_rate) / (precision_rate + recall_rate);
+    end
 
 end
 
