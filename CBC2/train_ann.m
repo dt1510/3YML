52f90a5a6743afeed21faa69d2705723de4f5f10
David Toth <dt1510@ic.ac.uk>
2012-11-21 14:04:57 +0000

    Setting net.divideFcn = 'divideint' in train_ann.m.

--- CBC2/train_ann.m
+++ CBC2/train_ann.m
@@ -2,10 +2,11 @@ function [ net, tr ] = train_ann( net, x, y)
 %TRAIN_ANN
     unconfigure(net);
     [x2, y2] = ANNdata(x, y);
-    [net] = configure(net, x2, y2);    
+    [net] = configure(net, x2, y2);
+    net.divideFcn = 'divideint';
     [net] = split_training_val(net);
     %net.trainParam.epochs = 2;
-    net.trainParam.time = 20;
+    %net.trainParam.time = 20;
     [net, tr] = train(net, x2, y2);    
 end
 
