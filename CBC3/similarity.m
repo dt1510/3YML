06ab3eba9ca8a1931401acb9e08f9304a9b87114
David Toth <dt1510@ic.ac.uk>
2012-11-28 10:34:19 +0000

    retrieve function added.

--- CBC3/similarity.m
+++ CBC3/similarity.m
@@ -1,7 +1,8 @@
 function [ similarity_distance ] = similarity( single_case1, single_case2 )
 %SIMILARITY_FUNCTION - a very simple similarity function that may get
 %replaced in the future by another one taking into consideration
-%information entropy for example.
+%information entropy for example. The lower the number, the more similar
+%they are.
     similarity_distance = nnz(single_case1.x - single_case2.x);
 end
 
