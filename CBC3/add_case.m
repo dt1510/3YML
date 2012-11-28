d518842cfc0d812da453ac9db2be615bbed11f91
vinea <va909@matrix23.doc.ic.ac.uk>
2012-11-28 16:20:16 +0000

    typicality changed

--- CBC3/add_case.m
+++ CBC3/add_case.m
@@ -3,12 +3,11 @@ function [ case_struct ] = add_case( case_struct, single_case )
 
     for i = 1:size(case_struct.cases,2)
         if similarity(single_case,case_struct.cases(i))==0 && single_case.y == case_struct.cases(i).y
-            case_struct.typicality(i) = case_struct.typicality(i) + 1;
+            case_struct.cases(i).typicality = case_struct.cases(i).typicality + 1;
             return;
         end
     end
 
     case_struct.cases = [case_struct.cases single_case];
-    case_struct.typicality = [case_struct.typicality 1];
 end
 
--- CBC3/new_case.m
+++ CBC3/new_case.m
@@ -1,6 +1,6 @@
 function [ single_case ] = new_case( input_vector, target_value )
 %CASE generates a case structure. If there is a new case with no associated
 %target value, set the value to -1.
-    single_case = struct('x', input_vector, 'y', target_value);
+    single_case = struct('x', input_vector, 'y', target_value, 'typicality', 1);
 end
 
