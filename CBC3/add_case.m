95eea13f0ae678015af277f5e10d8797e7fbad61
vinea <va909@matrix23.doc.ic.ac.uk>
2012-11-28 15:48:37 +0000

    add_case duplicates condition is buggy

--- CBC3/add_case.m
+++ CBC3/add_case.m
@@ -1,11 +1,11 @@
 function [ case_struct ] = add_case( case_struct, single_case )
 %should try to add a case but also avoid duplicates
 
-  %  for i = 1:size(case_struct.cases,1)
-  %      if single_case == case_struct.cases(i)
-   %         return;
-  %      end
-  %  end
+    for i = 1:size(case_struct.cases,2)
+        if (single_case.x == case_struct.cases(i).x && single_case.y == case_struct.cases(i).y)
+           return
+        end
+    end
     case_struct.cases = [case_struct.cases single_case];
 end
 
