94077091fa880bf2c898e4aba15be41e385d0f4b
vinea <va909@matrix23.doc.ic.ac.uk>
2012-11-28 16:03:17 +0000

    typicality added

--- CBC3/add_case.m
+++ CBC3/add_case.m
@@ -2,10 +2,12 @@ function [ case_struct ] = add_case( case_struct, single_case )
 %should try to add a case but also avoid duplicates
 
     for i = 1:size(case_struct.cases,2)
-        if (single_case.x == case_struct.cases(i).x && single_case.y == case_struct.cases(i).y)
-           return
+        if similarity(single_case,case_struct.cases(i))==0 && single_case.y == case_struct.cases(i).y
+            case_struct.typicality(i) = case_struct.typicality(i) + 1;
+            return;
         end
     end
     case_struct.cases = [case_struct.cases single_case];
+    case_struct.typicality = [case_struct.typicality 1];
 end
 
