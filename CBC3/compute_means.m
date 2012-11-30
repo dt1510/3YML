8b5e159915f7c159909a11c68cafb80252f4c634
David Toth <dt1510@ic.ac.uk>
2012-11-30 20:50:39 +0000

    similarity_mean added.

--- CBC3/compute_means.m
+++ CBC3/compute_means.m
@@ -1,5 +1,7 @@
 function [ cbr ] = compute_means( cbr )
 %COMPUTE_MEANS computes mean (average) cases.
-    
+    for emotion = 1:6
+        cbr.mean_active(emotion, :) = cbr.active_count(emotion, :) / cbr.class_count(emotion);
+    end
 end
 
