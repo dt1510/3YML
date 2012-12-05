8ba9819b6402ad0553b20f04bbd1d05bfa31ee69
David Toth <dt1510@ic.ac.uk>
2012-12-05 13:26:07 +0000

    Added error rates to cross-validate.

--- CBC2/cross_validate.m
+++ CBC2/cross_validate.m
@@ -1,4 +1,4 @@
-function [ info ] = cross_validate( x, y, fold_number, net)
+function [ info, error_rates ] = cross_validate( x, y, fold_number, net)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -26,6 +26,24 @@ function [ info ] = cross_validate( x, y, fold_number, net)
     end
     [test_examples, ~] = ANNdata(data.test_examples, data.test_targets);
     info.predictions = testANN(net,test_examples);
+    
+    
+    error_rates = zeros(6, 1);
+    len = size(data.test_targets, 1);
+    len
+    bin_targets = zeros(len);
+    bin_predictions = zeros(len);
+    for emotion=1:6
+        for i=1:len
+            bin_targets(i) = data.test_targets(i) == emotion;
+            bin_predictions(i) = info.predictions(i) == emotion;
+        end       
+        
+        error_rate = nnz(bin_targets - bin_predictions)/len;        
+        error_rates(emotion)=error_rate;
+    end    
+    
+    
     info.confusion_matrix = get_confusion_matrix(num_classes, data.test_targets, info.predictions);
     disp(info.confusion_matrix);
     info.error_rate = get_error_rate(data.test_targets, info.predictions);
